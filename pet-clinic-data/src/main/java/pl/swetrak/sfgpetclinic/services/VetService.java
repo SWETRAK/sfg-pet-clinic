28ff50a78a5fac25610eed3e13b5a8fa4b6e2f3f
SWETRAK <kamilpietrak123@gmail.com>
2022-05-08 20:29:24 +0200

    Implement Interfaces for services. Closed #5.

---
+++ pet-clinic-data/src/main/java/pl/swetrak/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package pl.swetrak.sfgpetclinic.services;
+
+import pl.swetrak.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+
+
+}
